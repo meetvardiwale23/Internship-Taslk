84f8de33ee5f7dcf3175c3200b177d59e9e2d092
meetvardiwale23 <meetvardiwale23@gmail.com>
2023-03-10 19:10:33 +0530

    todays 10th feb commit

--- Authentication node JS/app.js
+++ Authentication node JS/app.js
@@ -7,7 +7,7 @@ var cookieparse = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const util = require('util');
 const flash = require('flash');
-const port = 4030;
+const port = 4020;
 
 
 app.set("view engine", "ejs");
@@ -50,54 +50,140 @@ app.post('/submit',async(req,res)=>{
     var name = req.body.name;
     var email = req.body.email;
     var password = req.body.password;
-    var hashpass;
     var conform = req.body.conform_password;
-    console.log(name,email,password,conform);
+    //console.log(name,email,password,conform);
 
     await new Promise((resolve,reject)=>{
         bcrypt.genSalt(10,(err,salt)=>{
             bcrypt.hash(password,salt,(err,hash)=>{
                 if(err) reject(err);
                 resolve(hash);
+                
                 hashpass=hash;
-                console.log(hashpass)
+              //  console.log(hashpass)
     
             })
         })
     })
     
-
+    var insertData;
     await new Promise((resovle,reject)=>{
         con.query(`INSERT INTO user_table(user_name,email,user_password) values('${name}','${email}','${hashpass}');`,(err,data)=>{
             if(err) reject(err);
             resovle(data);
-            console.log(data);
+            //console.log(data);
+            insertData = data;
+        
         })
     })
 
-    res.send('registration done go on <a href="/login">Log In</a>');
+    
+    console.log(insertData.insertId);
+
+    var activationToken = jwt.sign({id: insertData.insertId},'a@#45^7*9');
+  
+    res.cookie('activation',activationToken);
+    res.render("Activation");
+    
     res.end();
 })
 
-app.get('/login',(req,res)=>{
+app.get('/generateActivation',async(req,res)=>{
+  
+    res.render('login');
+
+})
+
+//for link activation
+app.post('/active', async(req,res)=>{
+    var activeToken = req.cookies.activation;
+    var auth = req.cookies.auth;
+
+    console.log("inside Active");
+    console.log("Active token",auth);
+    
+
+    if(auth)
+    {
+        jwt.verify(auth,'secret',(err,active_token_data)=>{
+            if(err) throw err;
+                console.log(active_token_data);
+                console.log("asdd",active_token_data.id)
+                console.log(active_token_data.user_data[0].email); 
+
+                var sql =` UPDATE user_table SET isActive = 1 WHERE email = "${active_token_data.user_data[0].email}";`
+                console.log(sql);
+                con.query(sql,(err,update_data)=>{
+                if(err) throw err;
+                
+                console.log(update_data);
+                res.redirect('/dashboard');
+            });
+
+     
+        });
+        
+    }
+
+    else{
+        jwt.verify(activeToken,'a@#45^7*9',(err,active_token_data)=>{
+            if(err) throw err;
+                console.log(active_token_data);
+                console.log("asdd",active_token_data.id)
+                console.log(active_token_data.id); 
+
+
+                var sql =` UPDATE user_table SET isActive = 1 WHERE userid = "${active_token_data.id}";`
+                console.log(sql);
+                con.query(sql,(err,update_data)=>{
+                if(err) throw err;
+                
+                console.log(update_data);
+                res.redirect('/login');
+            });
+
+     
+        });
+        
+    }
+})
+
+
+app.get('/login',async(req,res)=>{
     var token =req.cookies.auth;
+    var activeToken = req.cookies.activation;
+
+    if(activeToken)
+    {
+        res.render('login');
+    }
+    else{
+        res.render('/generateActivation');
+    }
+
     if(!token){
+        
         res.render('login');
     }
     else{
         res.redirect('/dashboard')
     }
+    
 })
+
+app.get(`/verify/email`,async(req,res)=>{
+    var query = util.promisify(con.query).bind(con);
+    var user_data  =  await query(`SELECT * FROM user_table`);
+    
+    res.json(user_data.map(item=>item.email));
+});
+
+
 
 app.post('/onlogin',async(req,res)=>{
 
     var query = util.promisify(con.query).bind(con);
     var user_data  = await query(`SELECT * FROM user_table WHERE email = "${req.body.email}";`);
-    if(user_data.length == 0 )
-    {
-        res.send("The email dosent exist");
-        res.end();
-    }
     var isMatch = bcrypt.compareSync(req.body.password,user_data[0].user_password);
 
     
@@ -106,6 +192,7 @@ app.post('/onlogin',async(req,res)=>{
     if(isMatch)
     {   
         console.log("match");
+        
         var token = jwt.sign({user_data},'secret');
         console.log(token);
         res.cookie('auth',token);
@@ -114,21 +201,48 @@ app.post('/onlogin',async(req,res)=>{
     else{
         res.send("Your password is incorrect");
     }    
-
+    
 });
 
 app.get('/dashboard',async(req,res)=>{
     var token =req.cookies.auth;
     var token_name;
-    if(token)
-    {
-        jwt.verify(token,'secret',(err,token_data)=>{
+
+    
+        if(token)
+        {
+        jwt.verify(token,'secret',async(err,token_data)=>{
             if(err) throw err;
+          //  console.log("you token data",token_data);
+
             token_name =token_data.user_data[0].user_name;
-        })
+
+            var query = util.promisify(con.query).bind(con);
+            
+            var user_data  = await query(`SELECT * FROM user_table WHERE email = "${token_data.user_data[0].email}";`);
+            console.log("select query",user_data);
+            console.log(user_data[0].isActive);
+
+            if(user_data[0].isActive == 1)
+            {
+                    res.render('dashboard',{token_name});        
+            } 
+            else{
+                res.redirect('/generateActivation');
+            }
+
+          
+            
+        })    
         
-        res.render('dashboard',{token_name});
+    }    
+    else{
+        res.redirect('login')
     }
+    
+
+   
+    
 
 });
 
